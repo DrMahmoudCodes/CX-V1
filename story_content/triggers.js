a194be1e9cc681ca2cf5a30604320278de2f16bf
DrMahmoudCodes <mahmoudhanwer@gmail.com>
2025-12-19 00:28:41 +0200

    Add files via upload

---
+++ story_content/triggers.js
@@ -0,0 +1,31 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "68bNBZIou9X":
+        Script1();
+        break;
+      case "6EPqnBoajj0":
+        Script2();
+        break;
+      case "6SEgqAE7j9D":
+        Script3();
+        break;
+      case "6Fk2Gw4MCzf":
+        Script4();
+        break;
+      case "5mX6VzwVUvi":
+        Script5();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+};
